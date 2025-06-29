6e266cd30289e9eb440370996e623e976d9e0961
Mourice-Adero <aderomourice7@gmail.com>
2025-06-29 14:02:09 +0300

    Done: Add task feature in ToDO list component

--- src/components/shifter-V2/ToDoList.tsx
+++ src/components/shifter-V2/ToDoList.tsx
@@ -1,15 +1,43 @@
-import { useState } from "react";
+import { useState, type FormEvent } from "react";
 import { BiCircle } from "react-icons/bi";
 import { CgClose } from "react-icons/cg";
 import { TiTick } from "react-icons/ti";
 
+type toDoTask = {
+  id: number;
+  title: string;
+  status: string;
+};
+
 const ToDoList = () => {
-  const [tasks, setTasks] = useState([
+  const [tasks, setTasks] = useState<toDoTask[]>([
     { id: 1, title: "Task 1", status: "Done" },
     { id: 2, title: "Task 2", status: "Pending" },
     { id: 3, title: "Task 2", status: "Done" },
     { id: 4, title: "Task 4", status: "Pending" },
   ]);
+  const [newTaskName, setNewTaskName] = useState("");
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTaskName(e.target.value);
+  };
+
+  const addTask = (e: FormEvent) => {
+    e.preventDefault();
+    if (newTaskName !== "") {
+      const newID = tasks.length + 1;
+      const newTask = {
+        id: newID,
+        title: newTaskName,
+        status: "Pending",
+      };
+      console.log(newTask.id);
+
+      setTasks([...tasks, newTask]);
+      setNewTaskName("");
+    }
+  };
+
   return (
     <div className="flex justify-center p-20">
       <div className="py-10 px-20 bg-linear-to-tl from-cyan-500 to-blue-500 rounded-3xl w-2/3">
@@ -30,13 +58,21 @@ const ToDoList = () => {
             </span>
           </p>
         </div>
-        <div className="py-3 text-white">
+        <form className="py-3 text-white flex justify-between ">
           <input
             type="text"
             placeholder="Add task..."
-            className="w-full border-b border-b-white p-2 focus:outline-none"
+            className="w-10/12 focus:outline-none border-b border-b-white"
+            value={newTaskName}
+            onChange={handleInput}
           />
-        </div>
+          <button
+            className="border border-gray-400 rounded p-2 bg-cyan-500 text-black"
+            onClick={addTask}
+          >
+            Add
+          </button>
+        </form>
         <ul>
           {tasks.map((task) => {
             return (
